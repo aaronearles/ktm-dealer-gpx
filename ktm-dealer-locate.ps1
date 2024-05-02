cba0eb57432691278c33612a1ffb369a449e9da9
Aaron Earles <aaron.earles@pediatrix.com>
2024-05-02 13:34:49 -0600

    init

---
+++ ktm-dealer-locate.ps1
@@ -0,0 +1,38 @@
+# Define the URI
+$uri_us = "https://www.ktm.com/en-us/find-a-dealer/_jcr_content/root/responsivegrid_1_col/dealersearch.dealers.json?latitude=37.09024&longitude=-95.712891&country=US&qualification="
+$uri_ca = "https://www.ktm.com/en-us/find-a-dealer/_jcr_content/root/responsivegrid_1_col/dealersearch.dealers.json?latitude=37.09024&longitude=-95.712891&country=CA&qualification="
+# Send a GET request to the URI and convert the JSON response to a PowerShell object
+$response = Invoke-WebRequest -Uri $uri_us | ConvertFrom-Json
+
+# Create a new XML document for the GPX file
+$gpx = New-Object System.Xml.XmlDocument
+
+# Create the root element
+$gpxRoot = $gpx.CreateElement("gpx")
+$gpxRoot.SetAttribute("version", "1.1")
+$gpxRoot.SetAttribute("creator", "PowerShell")
+$gpx.AppendChild($gpxRoot)
+
+# Iterate over the dealers
+foreach ($dealer in $response.data) {
+    # Create a new waypoint element with the latitude and longitude attributes
+    $wpt = $gpx.CreateElement("wpt")
+    $wpt.SetAttribute("lat", $dealer.geoCodeLatitude)
+    $wpt.SetAttribute("lon", $dealer.geoCodeLongitude)
+
+    # Add a name child element to the waypoint element
+    $name = $gpx.CreateElement("name")
+    $name.InnerText = $dealer.name
+    $wpt.AppendChild($name)
+
+    # Add a description child element to the waypoint element
+    $desc = $gpx.CreateElement("desc")
+    $desc.InnerText = $dealer.address
+    $wpt.AppendChild($desc)
+
+    # Append the waypoint element to the GPX document
+    $gpxRoot.AppendChild($wpt)
+}
+
+# Save the GPX document to a file
+$gpx.Save("dealers.gpx")
